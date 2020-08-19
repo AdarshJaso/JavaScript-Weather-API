f920f1bb697938b8b3c512ae9778d9939f838570
Adarsh Jasoliya <68543181+AdarshJaso@users.noreply.github.com>
2020-08-19 19:19:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var card = document.getElementById("xyz");
 var city = ["1275339", "1850144", "5128581", "1273294", "2643743", "703448", "524901"];
 
 var myRequest = new XMLHttpRequest();
-myRequest.open('GET', "http://api.openweathermap.org/data/2.5/group?id=" + city.join(',') + "&units=metric&APPID=220a14ce06da0b6e1ec72fc14d0e59e0");
+myRequest.open('GET', "https://api.openweathermap.org/data/2.5/group?id=" + city.join(',') + "&units=metric&APPID=220a14ce06da0b6e1ec72fc14d0e59e0");
 myRequest.onload = function() {
 
 	if (myRequest.status >= 200 && myRequest.status < 400) {
